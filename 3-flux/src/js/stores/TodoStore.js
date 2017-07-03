a943c2303c1bdfa1f2f6a3c012400120396138e1
apa yu <rx836@hotmail.com>
2017-07-03 12:57:10 +0800

    #11 react tutorial

--- 3-flux/src/js/stores/TodoStore.js
+++ 3-flux/src/js/stores/TodoStore.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from "events";
 
+import dispatcher from "../dispatcher";
+
 class TodoStore extends EventEmitter {
   constructor(){
     super();
@@ -32,8 +34,18 @@ class TodoStore extends EventEmitter {
   getAll(){
     return this.todos;
   }
+
+  handleActions(action){
+    // console.log("TodoStore received an action", action);
+    switch(action.type){
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+      }
+    }
+  }
 }
 
 const todoStore = new TodoStore;
-window.todoStore = todoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+window.dispatcher = dispatcher;
 export default todoStore;
