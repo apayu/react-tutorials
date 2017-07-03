6097d386b569bab7050b197893a1f6bb0cd9f5eb
apa yu <rx836@hotmail.com>
2017-07-04 00:40:19 +0800

    #14 react tutorial

--- 3-flux/src/js/client.js
+++ 3-flux/src/js/client.js
@@ -4,12 +4,17 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Layout from "./pages/Layout";
 import Todos from "./pages/Todos";
+import Favorites from "./pages/Favorites";
+import Settings from "./pages/Settings";
+
 const app = document.getElementById('app');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Layout}>
       <IndexRoute component={Todos}></IndexRoute>
+      <Route path="Favorites" component={Favorites}></Route>
+      <Route path="Settings" component={Settings}></Route>
     </Route>
   </Router>,
 app);
